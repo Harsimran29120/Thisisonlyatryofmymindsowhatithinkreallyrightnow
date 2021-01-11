a67af1396a61163ab4633966550f0098725c1118
Harsimran29120 <72358179+Harsimran29120@users.noreply.github.com>
2021-01-11 17:37:26 +0100

    Add files via upload

---
+++ precache-manifest.eaa74b07060c3f7d9a533426340a009e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6c11dd18f069c372dfedd5229602fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e459e16f099013999c4f",
+    "url": "/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "c0a4a7ed8246106563b1",
+    "url": "/static/css/3.42bd9434.chunk.css"
+  },
+  {
+    "revision": "c7761fc40c23d051f031",
+    "url": "/static/css/main.580115a7.chunk.css"
+  },
+  {
+    "revision": "e459e16f099013999c4f",
+    "url": "/static/js/2.d761bbd6.chunk.js"
+  },
+  {
+    "revision": "148709ddc638d9a155f2e9c428088f0a",
+    "url": "/static/js/2.d761bbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a4a7ed8246106563b1",
+    "url": "/static/js/3.20b8699e.chunk.js"
+  },
+  {
+    "revision": "c7761fc40c23d051f031",
+    "url": "/static/js/main.3dba55f4.chunk.js"
+  },
+  {
+    "revision": "01d108b395bcdf37a9a3",
+    "url": "/static/js/runtime-main.b4685d16.js"
+  },
+  {
+    "revision": "85f8745d712d011a336d3f3993932bcd",
+    "url": "/static/media/camera-stop.85f8745d.svg"
+  },
+  {
+    "revision": "299a8f6706ceab1535aecf69ccaa4754",
+    "url": "/static/media/camera.299a8f67.svg"
+  },
+  {
+    "revision": "8c9e061ef6737de90953e983b2f9799b",
+    "url": "/static/media/cuckoo-logo.8c9e061e.svg"
+  },
+  {
+    "revision": "8f5e792134c28e4d0c6334510d3034e0",
+    "url": "/static/media/fullscreen.8f5e7921.svg"
+  },
+  {
+    "revision": "1b1f0dc8daca05d7bb230891e4acb66b",
+    "url": "/static/media/hang-up.1b1f0dc8.svg"
+  },
+  {
+    "revision": "09d0b6c27ab9d2a540058ad888032b50",
+    "url": "/static/media/microphone-stop.09d0b6c2.svg"
+  },
+  {
+    "revision": "a23748387b7ae4f2e6377d5b70bcd02d",
+    "url": "/static/media/microphone.a2374838.svg"
+  },
+  {
+    "revision": "fff27c21f0f61fc6615b80644f9101e6",
+    "url": "/static/media/minimize.fff27c21.svg"
+  },
+  {
+    "revision": "0be3c651e1532de63056a20e2f341bde",
+    "url": "/static/media/ringtone.0be3c651.mp3"
+  },
+  {
+    "revision": "e7b143c04ff26c4c774900650b490c9b",
+    "url": "/static/media/share.e7b143c0.svg"
+  }
+]);
